1aac1182ab98a694523fac92943aee4e8e2ef5e5
Taher Jerbi <jerbi.taherr@gmail.com>
2023-04-11 00:04:14 +0100

    feat: extract comment body and subreddit from line

--- src/main/java/tn/insat/nlpstream/Stream.java
+++ src/main/java/tn/insat/nlpstream/Stream.java
@@ -9,6 +9,7 @@ import scala.Tuple2;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class Stream {
     public static void main(String[] args) throws InterruptedException {
@@ -21,17 +22,29 @@ public class Stream {
         JavaReceiverInputDStream<String> lines =
                 jssc.socketTextStream("localhost", 9999);
 
-        SentimentAnalyzer sentimentAnalyzer = SentimentAnalyzer.getInstance();
+        // extract text and subreddit
+        JavaPairDStream<String, String> textAndSubreddit = lines.mapToPair(Stream::extractTextAndSubreddit);
 
-        // analyze sentiment of each line
-        JavaDStream<Map<String, Integer>> sentiments = lines.map(sentimentAnalyzer::getSentiment);
-
-        sentiments.print();
-
+        textAndSubreddit.print();
 
         jssc.start();
         jssc.awaitTermination();
     }
 
+
+    public static Tuple2<String, String> extractTextAndSubreddit(String line){
+        System.out.println("line: " + line);
+        try {
+            String text = line.replaceAll("\"?,r\\/\\w+", "")
+                    .replaceAll("\\d+,\\w+,\\w+,\"?", "");
+            String subreddit = line
+                    .replaceAll("\\d+,\\w+,\\w+,\"?", "")
+                    .replaceAll(text, "");
+
+            return new Tuple2<String, String>(text, subreddit);
+        } catch (Exception e){
+            return new Tuple2<String, String>("", "");
+        }
+    }
 
 }
