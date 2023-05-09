d891a15e6c15593de5768b131c2cea882be66bc4
Taher Jerbi <jerbi.taherr@gmail.com>
2023-05-09 13:07:35 +0100

    feat: read and write to kafka

--- src/main/java/tn/insat/nlpstream/SentimentAnalyzer.java
+++ src/main/java/tn/insat/nlpstream/SentimentAnalyzer.java
@@ -5,6 +5,7 @@ import edu.stanford.nlp.pipeline.CoreSentence;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -24,10 +25,18 @@ public class SentimentAnalyzer implements Serializable {
         return instance;
     }
 
-    public Map<String, Integer> getSentiment(String text){
+    // add sentiment to each line
+    // IN: comment_id, comment_parent_id, comment_body, subreddit, timestamp
+    // OUT: comment_id, comment_parent_id, comment_body, subreddit, timestamp, sentiment
+    public String addSentiment(String text){
         // create a pipeline inside the function to avoid the error: java.io.NotSerializableException: edu.stanford.nlp.pipeline.StanfordCoreNLP
-        System.out.println("Text: "+text);
         StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        // extract body from the text
+        // take from the 2nd comma to the 2nd last comma
+        String[] parts = text.split(",");
+        String body = String.join(",", Arrays.copyOfRange(parts, 2, parts.length-2));
+        System.out.println("Body: "+body);
         // create a document object
         CoreDocument doc = new CoreDocument(text);
         // annotate
@@ -37,8 +46,8 @@ public class SentimentAnalyzer implements Serializable {
         // display sentences
         for (CoreSentence sentence : doc.sentences()) {
             String sentiment = sentence.sentiment();
-            System.out.println("Sentence: "+sentence.toString());
-            System.out.println("Sentiment: "+sentiment);
+            System.out.println("    Sentence: "+sentence.toString());
+            System.out.println("    Sentiment: "+sentiment);
 
             if(sentiments.containsKey(sentiment)){
                 sentiments.put(sentiment, sentiments.get(sentiment)+1);
@@ -46,6 +55,17 @@ public class SentimentAnalyzer implements Serializable {
                 sentiments.put(sentiment, 1);
             }
         }
-        return sentiments;
+
+        // get the majority sentiment from the sentiments map
+        String majoritySentiment = "";
+        int max = 0;
+        for(Map.Entry<String, Integer> entry : sentiments.entrySet()){
+            if(entry.getValue() > max){
+                max = entry.getValue();
+                majoritySentiment = entry.getKey();
+            }
+        }
+        System.out.println("Majority Sentiment: "+majoritySentiment);
+        return text + "," + majoritySentiment;
     }
 }
--- src/main/java/tn/insat/nlpstream/Stream.java
+++ src/main/java/tn/insat/nlpstream/Stream.java
@@ -1,36 +1,81 @@
 package tn.insat.nlpstream;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.spark.SparkConf;
 import org.apache.spark.streaming.Durations;
-import org.apache.spark.streaming.api.java.JavaDStream;
-import org.apache.spark.streaming.api.java.JavaPairDStream;
-import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
-import org.apache.spark.streaming.api.java.JavaStreamingContext;
-import scala.Tuple2;
+import org.apache.spark.streaming.api.java.*;
+import org.apache.spark.streaming.kafka.KafkaUtils;
 
-import java.util.Arrays;
+import scala.Tuple2;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 public class Stream {
     public static void main(String[] args) throws InterruptedException {
+        if (args.length < 4) {
+            System.err.println("Usage: SparkKafkaWordCount <zkQuorum> <group> <in-topics> <numThreads> <out-topic>");
+            System.exit(1);
+        }
+
         SparkConf conf = new SparkConf()
-                .setAppName("NetworkWordCount")
+                .setAppName("SparkSentimentAnalysis")
                 .setMaster("local[*]");
         JavaStreamingContext jssc =
                 new JavaStreamingContext(conf, Durations.seconds(5));
 
-        JavaReceiverInputDStream<String> lines =
-                jssc.socketTextStream("localhost", 9999);
+        int numThreads = Integer.parseInt(args[3]);
+        Map<String, Integer> topicMap = new HashMap<>();
+        String[] topics = args[2].split(",");
+        for (String topic: topics) {
+            topicMap.put(topic, numThreads);
+        }
+
+        JavaPairReceiverInputDStream<String, String> messages =
+                KafkaUtils.createStream(jssc, args[0], args[1], topicMap);
+
+        JavaDStream<String> lines = messages.map(Tuple2::_2);
+
 
         SentimentAnalyzer sentimentAnalyzer = SentimentAnalyzer.getInstance();
 
-        // analyze sentiment of each line
-        JavaDStream<Map<String, Integer>> sentiments = lines.map(sentimentAnalyzer::getSentiment);
-
-        sentiments.print();
+        // analyze the majority sentiment of each line and add it to the line
+        JavaDStream<String> comments_sentiments = lines.map(sentimentAnalyzer::addSentiment);
 
 
+        // only send if args[4] is not empty
+        if (args.length < 5) {
+            comments_sentiments.print();
+            jssc.start();
+            jssc.awaitTermination();
+            return;
+        }
+        String outputTopic = args[4];
+        JavaPairDStream<String, String> output = comments_sentiments.mapToPair(line -> new Tuple2<>("key", line));
+        output.foreachRDD(rdd -> {
+            rdd.foreachPartition(partition -> {
+                KafkaProducer<String, String> producer = createKafkaProducer();
+                while (partition.hasNext()) {
+                    Tuple2<String, String> record = partition.next();
+                    String value = record._2();
+                    ProducerRecord<String, String> producerRecord = new ProducerRecord<>(outputTopic, value);
+                    producer.send(producerRecord);
+                }
+                producer.close();
+            });
+        });
+
+        comments_sentiments.print();
         jssc.start();
         jssc.awaitTermination();
+    }
+
+    private static KafkaProducer<String, String> createKafkaProducer() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        return new KafkaProducer<>(props);
     }
 
 
